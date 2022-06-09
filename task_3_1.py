d6cb74891c2f9def68c905896ba915b25203b794
AlexBoronin <acivinel@mail.ru>
2022-06-09 12:01:30 +0300

    ПЗ №3 решено, добавлено описание в readme.txt

--- task_3_1.py
+++ task_3_1.py
@@ -1,39 +1,22 @@
 # Реализовать склонение слова «процент» во фразе "N процентов".
 # Вывести эту фразу на экран отдельной строкой для каждого из чисел в интервале от 1 до 150.
-# Формат вывода результата:
-# 0 процентов
-# 1 процент
-# 2 процента
-# 3 процента
-# 4 процента
-# 5 процентов
-# 6 процентов
-# и т.п.
-#
-# Алгоритм:
-# Ваш алгоритм должен корректно работать и для интервала от 1 до 150,
-# и для любого числа(заведенного в код), например от 1 до 1000.
-# Правила склонения здесь достаточно просты: всего три варианта:
-# «процент»/«процента»/«процентов».
-# Обратите внимание на диапазон 10-19 и как он повторяется.
 
 # number = int(input('Please select an option: 1 - number or 2 - interval (enter 1 or 2): '))
 # var_0 = 'процентов'
 # var_1 = 'процент'
 # var_2 = 'процента'
-
+#
 # if number == 1:
 #     option_1 = input('Введите ваше число: ')
-#     if option_1[-1] == '1':
+#     if int(option_1) // 10 > 0 or int(option_1) % 10 == 0:
+#         if option_1[-2:] == '11' or '12' or '13' or '14' or '15' or '16' or '17' or '18' or '19':
+#             print(option_1, var_0)
+#         elif option_1[-1] == '2' or '3' or '4':
+#             print(option_1, var_2)
+#     elif option_1[-1] == '1':
 #         print(option_1, var_1)
-#     if option_1[-1] == '0':
+#     else:
 #         print(option_1, var_0)
-#     if option_1[-2:] == '11' or '12' or '13' or '14' or '15' or '16' or '17' or '18' or '19':
-#         print(option_1, var_0)
-#     if option_1[-1] == '2' or '3' or '4':
-#         print(option_1, var_2)
-#     # else:
-#     #     print(option_1, var_0)
 
 # if number == 2:
 #     option_2 = int(input('Введите интервал: '))
@@ -42,7 +25,7 @@
 #         digit += 1
 #         if str(digit)[-1] == '0':
 #             print(digit, var_0)
-#         elif str(digit)[:-2] == '11' or '12' or '13' or '14' or '15' or '16' or '17' or '18' or '19':
+#         elif str(digit)[-2:] == '11' or '12' or '13' or '14' or '15' or '16' or '17' or '18' or '19':
 #             print(digit, var_0)
 #         elif str(digit)[-1] == '1':
 #             print(digit, var_1)
@@ -51,13 +34,32 @@
 #         else:
 #             print(digit, var_0)
 
-a = 'asd'
-c = input()
-sb = b'a'
-ds = b'c'
-print(c)
-print(c[0])
-print(ds[0])
-print(a)
-print(a[0])
-print(sb[0])
+number = int(input('Please select an option: 1 - number or 2 - interval (enter 1 or 2): '))
+var_0 = 'процентов'
+var_1 = 'процент'
+var_2 = 'процента'
+
+if number == 1:
+    option_1 = int(input('Enter your digit: '))
+    if 20 > option_1 > 10 or option_1 % 100 == 1 and 20 > option_1 > 10 or option_1 % 10 == 0 or 20 > option_1 % 100 > 10:
+        print(option_1, var_0)
+    elif option_1 % 10 == 1 or (option_1 > 20 and option_1 % 10 == 1):
+        print(option_1, var_1)
+    elif 1 < option_1 < 5 and option_1 % 10 == option_1 or 5 > option_1 % 10 > 1:
+        print(option_1, var_2)
+    else:
+        print(option_1, var_0)
+
+if number == 2:
+    option_2 = int(input('Enter your period: '))
+    count = 0
+    while count != option_2:
+        count += 1
+        if 20 > count > 10 or count % 100 == 1 and 20 > count > 10 or count % 10 == 0 or 20 > count % 100 > 10:
+            print(count, var_0)
+        elif count % 10 == 1 or (count > 20 and count % 10 == 1):
+            print(count, var_1)
+        elif 1 < count < 5 and count % 10 == count or 5 > count % 10 > 1:
+            print(count, var_2)
+        else:
+            print(count, var_0)
